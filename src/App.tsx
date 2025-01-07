db99dcaa2794513c37f6ac70838d2d75f68ee6fc
Ajay <ajaykumawat9904@gmail.com>
2025-01-07 15:14:26 +0530

    feat(shadcn): configur shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-orange-500">Hello world!</h1>
+      <Button>Click me</Button>
     </>
   );
 }
